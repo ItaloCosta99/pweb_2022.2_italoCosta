5dfbe9cdf9706d5ce758325089c7ad4baed78650
Italo Costa <italocostajobs@gmail.com>
2023-02-27 15:30:16 -0300

    Chore: inicializando a aplicação com springboot

---
+++ u1_exercicio3/src/main/java/br/com/italocosta/cadpessoas/CadpessoasItalocostaApplication.java
@@ -0,0 +1,13 @@
+package br.com.italocosta.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpessoasItalocostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpessoasItalocostaApplication.class, args);
+	}
+
+}
---
+++ u1_exercicio3/src/test/java/br/com/italocosta/cadpessoas/CadpessoasItalocostaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.italocosta.cadpessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadpessoasItalocostaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
