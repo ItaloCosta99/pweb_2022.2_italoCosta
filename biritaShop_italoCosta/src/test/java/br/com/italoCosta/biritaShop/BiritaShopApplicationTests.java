21f255e68f2c4f9bc05e9757ab3306cc87ea4f9f
Italo Costa <italocostajobs@gmail.com>
2023-04-15 16:36:44 -0300

    feat: iniciando spring application BiritaShop

---
+++ biritaShop_italoCosta/src/test/java/br/com/italoCosta/biritaShop/BiritaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.italoCosta.biritaShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiritaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
